60e062da8ea4840fdf6da4341ebb4fc6579c02e0
Dylan Vann <dylan@dylanvann.com>
2018-07-16 14:39:52 -0400

    Add notes on formats.

--- src/commonFields.js
+++ src/commonFields.js
@@ -5,9 +5,17 @@ export const commonFields = {
     originalName: { type: GraphQLString },
     aspectRatio: { type: GraphQLFloat },
     base64: { type: GraphQLString },
-    // ------------------------------
-    // formats
-    // ------------------------------
+    /**
+     * Formats.
+     *
+     * The way this module works these different formats are generated based
+     * on the user's query.
+     *
+     * So if the user does not include srcWebM in their query then a webM version
+     * of the asset is not generated.
+     *
+     * This node will also only upload the image once, then perform the transformations.
+     */
     // src
     src: { type: GraphQLString },
     srcSet: { type: GraphQLString },
