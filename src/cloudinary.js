0d6f59f1ba9c727e805e8316b72379287c2c06e3
Dylan Vann <dylan@dylanvann.com>
2018-07-16 23:59:24 -0400

    Remove dead code.

--- src/cloudinary.js
+++ src/cloudinary.js
@@ -11,31 +11,3 @@ export const upload = cloudinary => (id, absolutePath) =>
             },
         ),
     )
-
-export const transform = cloudinary => (id, options) =>
-    new Promise((resolve, reject) => {
-        cloudinary.v2.uploader.explicit(
-            id,
-            {
-                type: 'upload',
-                eager: [options],
-            },
-            (error, result) => {
-                if (error) reject(error)
-                resolve(result)
-            },
-        )
-    })
-
-export const checkImage = cloudinary => id =>
-    new Promise((resolve, reject) => {
-        cloudinary.v2.uploader.explicit(
-            id,
-            { type: 'upload' },
-            (error, result) => {
-                console.log('check', result)
-                if (error) reject(error)
-                resolve(result)
-            },
-        )
-    })
