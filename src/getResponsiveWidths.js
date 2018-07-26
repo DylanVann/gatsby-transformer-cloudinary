4b227daa64ac75f876334d352c51216144a6ac55
Dylan Vann <dylan@dylanvann.com>
2018-07-25 21:40:44 -0400

    Fix very large sizes.

--- src/getResponsiveWidths.js
+++ src/getResponsiveWidths.js
@@ -1,12 +1,16 @@
 // This is enough sizes to provide close to the optimal image size for every
 // device size / screen resolution.
 // We don't upscale the images.
-const responsiveWidthFactors = [0.25, 0.5, 1, 1.5, 2, 3]
+// We're considering users may use max widths up to 4k (3,840px).
+// So we need sizes down to much smaller sizes.
+const responsiveWidthFactors = [0.05, 0.1, 0.25, 0.5, 1, 1.5, 2, 3]
 
 export const getResponsiveWidths = (maxWidth, width) =>
     responsiveWidthFactors
         // Getting widths at different factors.
         .map(factor => Math.round(factor * maxWidth))
+        // Filter out really small sizes.
+        .filter(w => w > 200)
         // Filtering out widths greater than our original width.
         // We only want widths smaller than the original size.
         .filter(w => w < width)
